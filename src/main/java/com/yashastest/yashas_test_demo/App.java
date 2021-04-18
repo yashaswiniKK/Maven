9b8a2231aeb827687ab96d7bc1633539ab0ca552
yashaswiniKK <yashaswinikariappa12@gmail.com>
2021-04-18 21:51:34 +0530

    adding initial gitignore files

---
+++ src/main/java/com/yashastest/yashas_test_demo/App.java
@@ -0,0 +1,13 @@
+package com.yashastest.yashas_test_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
